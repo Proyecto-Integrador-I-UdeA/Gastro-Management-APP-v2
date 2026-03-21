24f971cfad6039c128eca5d3f341887b38e8be40
David Naranjo <guillermo.naranjo@udea.edu.co>
2026-03-21 16:49:09 -0500

    Fix CORS for Vercel frontend

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -7,8 +7,13 @@ import productRoutes from './routes/products/index';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://gastro-management-app-v2.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
